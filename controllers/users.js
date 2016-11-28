narg/contacts-list-api

Initial template

controllers/users.js
@@ -1,0 +1,40 @@
+var User = require('../models/user');
+var express = require('express');
+var router = express.Router();
+
+
+// GET /users
+// Get a list of users
+router.get('/', function(req, res) {
+  User.find({}, function(err, users) {
+    if (err) {
+      return res.status(500).json({
+        error: "Error listing users: " + err
+      });
+    }
+
+    res.json(users);
+  });
+});
+
+// GET /users/:id
+// Get a user by ID
+router.get('/:id', function(req, res) {
+  User.findOne({
+    _id: req.params.id
+  }, function(err, user) {
+    if (err) {
+      return res.status(500).json({
+        error: "Error reading user: " + err
+      });
+    }
+
+    if (!user) {
+      return res.status(404).end();
+    }
+
+    res.json(user);
+  });
+});
+
+module.exports = router;

sarahbarron/git-tutorial

controllers/users.js
@@ -1,0 +1,41 @@
+var UserModel = require('../models/user');
+var User = new UserModel();
+var express = require('express');
+var router = express.Router();
+
+
+// GET /users
+// Get a list of users
+router.get('/', function(req, res) {
+  User.find({}, function(err, users) {
+    if (err) {
+      return res.status(500).json({
+        error: "Error listing users: " + err
+      });
+    }
+
+    res.json(users);
+  });
+});
+
+// GET /users/:username
+// Get a user by ID
+router.get('/:username', function(req, res) {
+  User.findOne({
+    username: req.params.username
+  }, function(err, user) {
+    if (err) {
+      return res.status(500).json({
+        error: "Error reading user: " + err
+      });
+    }
+
+    if (!user) {
+      return res.status(404).end();
+    }
+
+    res.json(user);
+  });
+});
+
+module.exports = router;